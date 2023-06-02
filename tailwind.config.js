5f9bcddd52747cc5a0fc6267290353bdfd0582d8
andimahathir <enlandiy@ymail.com>
2023-06-02 16:56:56 +0700

    force light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ const forms = require('@tailwindcss/forms');
 
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', 'node_modules/preline/dist/*.js'],
-  darkMode: 'media',
+  darkMode: 'class',
   plugins: [forms, require('preline/plugin')],
 };
