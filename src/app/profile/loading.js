ea73177fb7903cf17a773977932b46980046c537
andimahathir <enlandiy@ymail.com>
2023-06-04 16:41:19 +0700

    fuck eslint

--- src/app/profile/loading.js
+++ src/app/profile/loading.js
@@ -1,11 +1,11 @@
 export default function Loading() {
   return (
     <div
-      class="flex h-8 w-8 animate-spin items-center justify-center rounded-full border-[3px] border-current border-t-transparent text-blue-600"
+      className="flex h-8 w-8 animate-spin items-center justify-center rounded-full border-[3px] border-current border-t-transparent text-blue-600"
       role="status"
       aria-label="loading"
     >
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 }
